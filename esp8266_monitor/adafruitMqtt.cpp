592b23fce8edf18e149e6d2b2bab4b02576f41f2
Kaz Okuda <kaz@okuda.ca>
2020-08-05 22:21:31 -0700

    Added "depth" configuration parameter and telemetry data

--- esp8266_monitor/adafruitMqtt.cpp
+++ esp8266_monitor/adafruitMqtt.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <WiFiClient.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
+#include <mem.h>
 
 // Define the following macros in a file called "configAdafruitMqtt.h"
 // Do not check this file into git as it may contain passwords
@@ -19,11 +21,16 @@ static Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNA
 static Adafruit_MQTT_Publish mqttTemperature = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/watermonitor.temperature");
 static Adafruit_MQTT_Publish mqttHumidity = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/watermonitor.humidity");
 static Adafruit_MQTT_Publish mqttDistance = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/watermonitor.distance");
+static Adafruit_MQTT_Publish mqttDepth = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/watermonitor.depth");
 
 static Adafruit_MQTT_Subscribe mqttInterval = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/watermonitor.interval");
 static Adafruit_MQTT_Publish mqttIntervalRequest = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/watermonitor.interval/get");
 
+static Adafruit_MQTT_Subscribe mqttConfiguration = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/watermonitor.configuration");
+static Adafruit_MQTT_Publish mqttConfigurationRequest = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/watermonitor.configuration/get");
+
 static int16_t _interval = 0;
+static int16_t _maxDepth = 160;
 
 static void intervalCallback(uint32_t interval) {
     Serial.printf("Interval is %u\r\n", interval);
@@ -34,9 +41,38 @@ static void intervalCallback(uint32_t interval) {
     }
 }
 
+static void configurationCallback(char* config, uint16_t len) {
+
+    Serial.print(F("Configuration is "));
+    Serial.print(config);
+    Serial.print(F(" "));
+    Serial.println(len);
+
+    StaticJsonDocument<200> doc;
+    auto error = deserializeJson(doc, config);
+    if (error) {
+        Serial.printf_P("Failed to parse configuration");
+        return;
+    }
+
+    JsonObject configObject = doc.as<JsonObject>();
+
+    // depth value represents the depth in cm of the water if the sensor were at 0cm.
+    // depth - distance to sensor ~= depth of water
+    if (configObject.containsKey(F("depth"))) {
+        int16_t depth = configObject[F("depth")].as<int16_t>();
+        _maxDepth = depth;
+    } else {
+        Serial.println(F("depth not in config"));
+    }
+}
+
 void adafruitMqttSetup() {
     mqttInterval.setCallback(intervalCallback);
     mqtt.subscribe(&mqttInterval);
+
+    mqttConfiguration.setCallback(configurationCallback);
+    mqtt.subscribe(&mqttConfiguration);
 }
 
 bool adafruitMqttConnect() {
@@ -65,6 +101,7 @@ bool adafruitMqttConnect() {
     // Publishing to feeds/xxx/get will trigger an update to feeds/xxx
     // https://io.adafruit.com/api/docs/mqtt.html#using-the-get-topic
     mqttIntervalRequest.publish("");
+    mqttConfigurationRequest.publish("");
 
     return true;
 }
@@ -83,6 +120,11 @@ void adafruitMqttPublish(float distance, float temperature, float humidity) {
     if (distance > 0) {
         if (mqttDistance.publish(distance)) {
             Serial.println(F("Sending distance..."));
+        }
+
+        int16_t depth = _maxDepth - distance;
+        if (mqttDepth.publish(depth)) {
+            Serial.println(F("Sending depth..."));
         }
     }
 
