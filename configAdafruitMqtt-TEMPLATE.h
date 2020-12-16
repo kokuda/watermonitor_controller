45039c9fb4c8db78cdfeee90fb4cda41effc8b36
Kaz Okuda <kaz@okuda.ca>
2020-12-15 21:13:01 -0800

    Added config*-TEMPLATE.h  files as examples of what the config files should look like

---
+++ configAdafruitMqtt-TEMPLATE.h
@@ -0,0 +1,4 @@
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_USERNAME    "Your Adafruit account name"
+#define AIO_KEY         "Your Adafruit key"
+#define AIO_SERVERPORT  1883                   
