8690a23ffad7b2f12315145a7850822bc8ca6a9e
Kaz Okuda <kaz@okuda.ca>
2021-08-05 23:06:18 -0700

    Added vaildateDistance to ignore bad data coming from distance sensor

--- ultrasonic.h
+++ ultrasonic.h
@@ -2,5 +2,10 @@
 // Returns the distance travelled by sound in ping_us microseconds at an air temperature of temperature
 float getDistance(unsigned int ping_us, float temperature);
 
+// The distance sensor sometimes returns very short distances.
+// This will ignore any values that are too different between calls and return the previous value
+// until the data is valid
+float validateDistance(float distance, unsigned long nowMs);
+
 // Returns the ping time in microseconds from the ultrasonic sensor
 unsigned int getUltrasonicSensorPingTimeUs();
